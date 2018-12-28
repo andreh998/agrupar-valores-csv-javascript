b983592901f82c0e7df6e48d52d4a783315522b8
André Hoffmann <andreh998@gmail.com>
2018-12-27 22:14:52 -0200

    agrupando os dados repetidos

---
+++ app.js
@@ -0,0 +1,16 @@
+var express = require('express');
+
+var app = express();
+
+app.use(express.static('assets'));
+
+app.set('view engine', 'ejs');
+app.set('views', './app/views');
+
+app.get('/', function(req, res){
+    res.render('home.ejs');
+});
+
+app.listen(8000, function(){
+    
+});
